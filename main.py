8fb68aba71c8d63b4ea9686b34526427a706299b
Jason Ashton <jlashton@wpi.edu>
2016-12-29 16:03:31 -0500

    can switch between  hardcoded files

--- main.py
+++ main.py
@@ -5,33 +5,42 @@ from PIL import ImageTk, Image
 class Application(tk.Frame):
     def __init__(self, master=None, imgPath=""):
         super().__init__(master)
-        self.imgPath = imgPath
+        self.imgPath = tk.StringVar()
+        self.imgPath.set(imgPath)
         self.pack()
-        self.create_widgets(self.imgPath)
+        self.create_widgets()
 
 
-    def create_widgets(self, imgPath):
-        self.img = ImageTk.PhotoImage(Image.open(imgPath))
+
+
+    def create_widgets(self):
+        self.img = ImageTk.PhotoImage(Image.open(self.imgPath.get()))
         self.imgPanel = tk.Label(image = self.img)
         self.imgPanel.pack(side="top")
 
+
+        self.test = tk.Label(self)
+        self.test["textvariable"] = self.imgPath
+        self.test.pack(side="top")
+
         self.prev = tk.Button(self)
         self.prev["text"] = "<"
-        self.prev["command"] = self.prev_img
+        self.prev["command"] = lambda: self.update_img("images/barn.jpg")
         self.prev.pack(side="left")
 
         self.next = tk.Button(self)
         self.next["text"] = ">"
-        self.next["command"] = self.next_img
+        self.next["command"] = lambda: self.update_img("images/mountain.jpg")
         self.next.pack(side="right")
 
 
-    def prev_img(self):
-        print("Loading Prev Image")
+    def update_img(self,img):
+        print("Loading Image: {}".format(img))
+        self.imgPath.set(img)
+        self.img = ImageTk.PhotoImage(Image.open(self.imgPath.get()))
+        self.imgPanel.configure(image = self.img)
 
 
-    def next_img(self):
-        print("Loading Next Image")
 
 if __name__ == "__main__":
 
